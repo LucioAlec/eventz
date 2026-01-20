83db76564278bb3af6dc084848be199b7a205bd1
LucioAlec <lucioalecpro@gmail.com>
2026-01-20 14:57:02 -0300

    Add edit and delete user account features

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,10 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update]
   def index
     @users = User.all
   end
 
-  def show
-    @user = User.find(user_params)
-  end
+  def show ; end
 
   def new
     @user = User.new
@@ -20,10 +19,31 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit ; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: "Account successfully updated!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path, status: :see_other,
+    alert: "Account successfully deleted!"
+  end
+
   private
 
   def user_params
-    params.require(:user).
-      permit(:name, :email, :password, :password_confirmation)
+    params.require(:user)
+      .permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
